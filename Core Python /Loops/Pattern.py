fd7f0064e4b67816d2dcf7b8dd493e1ccc7fde59
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-08-27 08:32:39 +0530

    Create Pattern.py

---
+++ Core Python /Loops/Pattern.py
@@ -0,0 +1,16 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(n,i,-1):
+        print(" ",end=" ")
+
+        
+    
+    k=i
+    for j in range(1,i+1):
+        print(k,end=" ")
+        k=k+1
+    p=2*(i-1)
+    for j in range(i,1,-1):
+        print(p,end=" ")
+        p=p-1
+    print("")
