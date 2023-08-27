fe5ad23484bc24b3f63b76fdf05f39cca016e0d4
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-08-27 10:57:36 +0530

    Create Binary Pattern.py

---
+++ Core Python /Loops/Binary Pattern.py
@@ -0,0 +1,17 @@
+''' Print Pattern-
+    1111
+    000
+    11 
+    0
+
+                 
+'''
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(n,i-1,-1):
+        if i%2==0:
+            print("0",end=" ")
+        else:
+            print("1",end=" ")
+    print(" ")
