be29120d2c06bbcbc8eb6898ed9bcf8dd9875338
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-08-29 12:21:15 +0530

    Create Checkfibonacci.py

---
+++ Core Python /Functions/Checkfibonacci.py
@@ -0,0 +1,20 @@
+
+def checkfibonacci(n):
+    if n==0 or n==1 or n==2:
+        return True
+    a=0
+    b=1
+    while b<n:
+        c=a+b
+        a=b
+        b=c
+        
+    if b==n:
+        return True
+    return False
+
+n=int(input())
+if checkfibonacci(n):
+    print("true")
+else:
+    print("false")
