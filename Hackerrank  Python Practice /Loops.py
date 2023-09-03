9ac1a919511eb706c28c6b346c6f594ba7b9a500
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-03 21:29:18 +0530

    Create Loops.py

---
+++ Hackerrank  Python Practice /Loops.py
@@ -0,0 +1,8 @@
+#Link-https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+n=int(input())
+li=[]
+for i in range(n):
+    li.append(i)
+for i in li:
+    print(i*i)
+    
