42fffdcadea9f6655be96991d137afdf889f6683
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-03 21:58:05 +0530

    Create Write a function.py

---
+++ Hackerrank  Python Practice /Write a function.py
@@ -0,0 +1,20 @@
+#Link->https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%4==0:
+        leap=True
+    if year%100==0:
+        leap=False
+    if year%400==0:
+        leap=True
+        
+        
+
+        
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
