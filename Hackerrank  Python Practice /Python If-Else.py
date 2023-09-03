f1db3bb67793d10ab01fb855eb8c552300c03ebf
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-03 21:02:27 +0530

    Create Python If-Else.py

---
+++ Hackerrank  Python Practice /Python If-Else.py
@@ -0,0 +1,11 @@
+# Link-https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+n=int(input())
+if n%2==1:
+    print("Weird")
+else:
+    if n>=2 and n<=5:
+        print("Not Weird")
+    elif n>=6 and n<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
