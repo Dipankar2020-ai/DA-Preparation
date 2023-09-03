fb97dde26833f1804bde50b8ff129508a9521e1e
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-03 21:05:03 +0530

    Create Arithmetic Operators.py

---
+++ Hackerrank  Python Practice /Arithmetic Operators.py
@@ -0,0 +1,8 @@
+#Link-https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+a=int(input())
+b=int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
+ 
