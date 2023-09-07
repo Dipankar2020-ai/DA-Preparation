7242c3341f08afbcd40c06a069d9ca42bc928371
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-07 08:54:55 +0530

    Create Insertion sort.py

---
+++ Core Python /Searching and Sorting/Insertion sort.py
@@ -0,0 +1,12 @@
+
+n=int(input())                                
+arr=[int(i) for i in input().split()]
+
+for i in range(1,len(arr)):
+    key=arr[i]
+    j=i-1
+    while j>=0 and key<arr[j]:
+        arr[j+1]=arr[j]
+        j=j-1
+    arr[j+1]=key
+print(arr)
