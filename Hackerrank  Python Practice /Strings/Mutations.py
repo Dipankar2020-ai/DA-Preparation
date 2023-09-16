44c36ddb0d43388edbbc2abbb404910bf07ba157
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-09-16 23:52:14 +0530

    Create Mutations.py

---
+++ Hackerrank  Python Practice /Strings/Mutations.py
@@ -0,0 +1,10 @@
+#Link-https://www.hackerrank.com/challenges/python-mutations/problem?isFullScreen=true
+def mutate_string(string, position, character):
+    string=string[0:position]+character+string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
