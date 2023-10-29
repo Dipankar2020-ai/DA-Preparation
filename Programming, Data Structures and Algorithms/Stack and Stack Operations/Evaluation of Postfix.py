e0349243c95253547054c5c7a3167a3dee650886
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2023-10-29 17:36:21 +0530

    Create Evaluation of Postfix.py

---
+++ Programming, Data Structures and Algorithms/Stack and Stack Operations/Evaluation of Postfix.py
@@ -0,0 +1,32 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+class Evaluation:
+    def __init__(self):
+        self.stack=[]
+    def isEmpty(self):
+        return self.stack==[]
+    def push(self,op):
+        self.stack.append(op)
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
+        else:
+            return '$'
+    def evaluatePostfix(self,exp):
+        for c in exp:
+            if c.isdigit():
+                self.push(c)
+            else:
+                a=self.pop()
+                b=self.pop()
+                self.push(str(eval(b+c+a)))
+        return int(self.pop())
+e=Evaluation()
+print(e.evaluatePostfix('234*6*+'))
+    
